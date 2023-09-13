fa6a88c32351505c8bbf8328a82e2569130c4f86
Breno Oliveira <brenooliveiraa@gmail.com>
2023-09-13 15:35:24 -0300

    organizando

--- ex4.py
+++ ex4.py
@@ -1,39 +1,62 @@
+# Importe as bibliotecas necessárias
 import pandas as pd
 import numpy as np
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from datetime import datetime
+
+# Define a URI do conjunto de dados
 uri = 'https://gist.githubusercontent.com/guilhermesilveira/4d1d4a16ccbf6ea4e0a64a38a24ec884/raw/afd05cb0c796d18f3f5a6537053ded308ba94bf7/car-prices.csv'
+
+# Carrega os dados do CSV
 dados = pd.read_csv(uri)
-torf={
-    'yes':1,
-'no': 0
+
+# Mapeia a coluna 'sold' de 'yes' e 'no' para 1 e 0
+torf = {
+    'yes': 1,
+    'no': 0
 }
 dados.sold = dados.sold.map(torf)
-x = dados.drop(columns = ["sold"], axis=1 )
-y= dados['sold']
 
-from datetime import datetime
+# Separa as features (variáveis independentes) e o target (variável dependente)
+x = dados.drop(columns=["sold"], axis=1)
+y = dados['sold']
+
+# Calcula a idade dos modelos dos carros e converte a quilometragem por ano para quilômetros
 currentYear = datetime.today().year
-dados['model_age'] = currentYear -dados.model_year
-dados["km_per_yar"] = dados.mileage_per_year * 1.60934
-dados = dados.drop(columns = ["Unnamed: 0", "mileage_per_year", "model_year"], axis = 1)
+dados['model_age'] = currentYear - dados.model_year
+dados["km_per_year"] = dados.mileage_per_year * 1.60934
+
+# Remove colunas não utilizadas
+dados = dados.drop(columns=["Unnamed: 0", "mileage_per_year", "model_year"], axis=1)
+
+# Exibe as primeiras linhas dos dados
 print(dados.head())
 
+# Define uma semente (seed) para reprodutibilidade dos resultados
 SEED = 20
 np.random.seed(SEED)
+
+# Divide os dados em conjuntos de treinamento e teste
 raw_train_x, raw_test_x, train_y, test_y = train_test_split(x, y, stratify=y, test_size=0.25)
 
+# Padroniza os dados usando StandardScaler
 scaler = StandardScaler()
 scaler.fit(raw_train_x)
 train_x = scaler.transform(raw_train_x)
 test_x = scaler.transform(raw_test_x)
 
-#acuracy padrao svc
+# Cria e treina o modelo de classificação SVM (Support Vector Machine)
 model = SVC()
 model.fit(train_x, train_y)
+
+# Realiza previsões no conjunto de teste
 previsoes = model.predict(test_x)
+
+# Calcula a acurácia do modelo
 acuracy = accuracy_score(test_y, previsoes) * 100
 
-print(f'a acuracia foi de {acuracy:.2f}')
+# Exibe a acurácia
+print(f'A acurácia foi de {acuracy:.2f}%')
---
+++ ex5.py
@@ -0,0 +1,7 @@
+#metodo KNN (baseado na próximidade)
+
+import pandas as pd
+
+dados = pd.read_csv('https://raw.githubusercontent.com/alura-cursos/ML_Classificacao_por_tras_dos_panos/main/Dados/Customer-Churn.csv')
+
+print(dados.head())
